dcaf0733e89d8fedff940fb9b6eed5e5126e9cd0
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2022-08-23 19:58:17 -0300

    nave movement added

--- main.js
+++ main.js
@@ -9,12 +9,24 @@ let highlandSize = { x:0, y: 0 }
 // M Mover a sonda.
 // P Aciona a camera.
 
+// adicionar linhas de entrada para tamanho do planalto [X]
 // adicionar a direção que a sonda esta apontada [X]
 // adicionar posição atual da sonda [X]
-// adicionar um movimento a sonda []
-// adicionar linhas de entrada para tamanho do planalto []
+// adicionar um movimento a sonda [X]
 // adicionar linhas de entrada para adicionar mais sondas []
 
+function workMovement() {
+
+    let movement = {
+        'S': () => cordinates.y--,
+        'W': () => cordinates.x--,
+        'E': () => cordinates.x++,
+        'N': () => cordinates.y++
+    }
+
+    movement[cordinates.d]()
+    console.log(cordinates)
+}
 
 function workDirections(inputDirection) {
 
@@ -25,7 +37,7 @@ function workDirections(inputDirection) {
             'N':'W',
             'W':'S'
         }
-        return changedDirection[cordinates.d];
+        cordinates.d = changedDirection[inputDirection];
     }
 
     if(inputDirection === 'R'){
@@ -35,21 +47,17 @@ function workDirections(inputDirection) {
             'N':'E',
             'E':'S'
         }
-        return changedDirection[cordinates.d];
+        cordinates.d = changedDirection[inputDirection];
     }
 }
 
 function workCordinates({ x, y, d }) {
     // if(x > highlandSize.x || x - 0) return;
     // if(y > highlandSize.y || y - 0) return;
-
-    const newX = Number(x) + cordinates.x;
-    const newY = Number(y) + cordinates.y;
-    const newD = d;
-    console.log(newX,newY,newD)
-    cordinates.x = newX;
-    cordinates.y = newY;
-    cordinates.d = newD;
+    
+    cordinates.x = Number(x) + cordinates.x;
+    cordinates.y = Number(y) + cordinates.y;
+    cordinates.d = d;
 }
 
 const readLine = createInterface({
@@ -58,25 +66,27 @@ const readLine = createInterface({
 })
 
 const questions = {
-    0 :() => readLine.question("Defina o tamanho do eixo x do planalto: ", answer => {
+    0: () => readLine.question("Defina o tamanho do eixo x do planalto: ", answer => {
         highlandSize.x = answer;
         questions[1]()
     }),
     
-    1 :() => readLine.question("Defina o tamanho do y do planalto: ", answer => {
+    1: () => readLine.question("Defina o tamanho do y do planalto: ", answer => {
         highlandSize.y = answer;
         questions[2]()
     }),
 
-    2 :() => readLine.question("Insira as cordenadas: ", answer => {
+    2: () => readLine.question("Defina as cordenadas da sonda: ", answer => {
         workCordinates({ 
             x: answer[0],
             y: answer[1],
             d: answer[2] 
         })
-    
-        console.log(cordinates)
-        readLine.close()
+        questions[3]()
+    }),
+
+    3: () => readLine.question("Defina o movimento da sonda: ", answer => {
+            workMovement()
     })
 }
 
