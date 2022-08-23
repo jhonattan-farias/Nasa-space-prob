dd1d210629ae9e8d0999725f52849df797d78c5a
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2022-08-23 12:22:05 -0300

    creating function to work the directions

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 const { createInterface } = require('readline');
 
-let cordinates = [];
+let cordinates = { x: 0, y: 0, d:'S' };
+
+// L R 90 graus
+// M MOver a sonda
+// P Aciona a camera
+
+function workDirections(inputDirection) {
+    if(inputDirection === 'L'){
+        switch(cordinates.d){
+            case 'S' :
+                return 'E'
+            case 'E' :
+                return 'N'
+            case 'N' :
+                return 'W' 
+            case 'W' :
+                return 'S'  
+        }
+    }
+
+    if(inputDirection === 'R'){
+        switch(cordinates.d){
+            case 'S' :
+                return 'W'
+            case 'W' :
+                return 'N'
+            case 'N' :
+                return 'E'
+            case 'E' :
+                return 'S'   
+        }
+    }
+}
+
+function workCordinates({ x, y, d }) {
+    return { x, y, d: workDirections(d) }
+}
 
 const readLine = createInterface({
     input:process.stdin,
@@ -8,7 +44,15 @@ const readLine = createInterface({
 })
 
 readLine.question("Insira as cordenadas: ", answer => {
-    cordinates.push(...answer)
+    const response = workCordinates({ 
+        x: answer[0],
+        y: answer[1],
+        d: answer[2] 
+    })
+
+
+    console.log(response)
     console.log(cordinates)
     readLine.close()
 })
+
