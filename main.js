abba6ba503b10c128d22d6727b58ea0447309485
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2022-08-24 15:50:35 -0300

    multiples movements orders created

--- main.js
+++ main.js
@@ -10,9 +10,12 @@ let highlandSize = { x:0, y: 0 }
 // P Aciona a camera.
 
 // adicionar linhas de entrada para tamanho do planalto [X]
-// adicionar a direção que a sonda esta apontada [X]
-// adicionar posição atual da sonda [X]
+    // garantir que seja recebido apenas numeros inteiros [X] 
+// adicionar cordenadas atuais da sonda [X]
+    // garantir que as entradas sejam inteiros positivos [X]
+    // garantir que a direção seja apenas (N S W E) [X]
 // adicionar um movimento a sonda [X]
+    // garantir que os movimentos da sonda recebidos sejam (M R L P) [X]
 // adicionar linhas de entrada para adicionar mais sondas [X]
 
 function workMovement(index) {
@@ -22,9 +25,7 @@ function workMovement(index) {
         'E': () => cordinates[index].x++,
         'N': () => cordinates[index].y++
     }
-
     movement[cordinates[index].d]()
-    console.log(cordinates[index])
 }
 
 function workDirections(inputDirection,index) {
@@ -36,7 +37,7 @@ function workDirections(inputDirection,index) {
             'N':'W',
             'W':'S'
         }
-        cordinates[index].d = changedDirection[inputDirection];
+        cordinates[index].d = changedDirection[cordinates[index].d];
     }
 
     if(inputDirection === 'R'){
@@ -46,7 +47,7 @@ function workDirections(inputDirection,index) {
             'N':'E',
             'E':'S'
         }
-        cordinates[index].d = changedDirection[inputDirection];
+        cordinates[index].d = changedDirection[cordinates[index].d];
     }
 }
 
@@ -65,36 +66,59 @@ const readLine = createInterface({
 
 const questions = {
     0: () => readLine.question("Defina o tamanho do eixo x do planalto: ", answer => {
+        if(answer < 0 || !Number(answer)){
+            console.log('Entrada inválida, digite novamente!')
+            questions[0]()
+        }
         highlandSize.x = answer
         questions[1]()
     }),
     
     1: () => readLine.question("Defina o tamanho do eixo y do planalto: ", answer => {
+        if(answer < 0 || !Number(answer)){
+            console.log('Entrada inválida, digite novamente!')
+            questions[0]()
+        }
         highlandSize.y = answer
         questions[2]()
     }),
 
-    2: () => readLine.question("Defina as cordenadas da sonda: ", answer => {
+    2: () => readLine.question("Defina as cordenadas da sonda ex(22N): ", answer => {
+
+        if(!Number(answer[0]) || !Number(answer[1])|| !['N','S','E','W'].includes(answer[2])){
+            console.log('Entrada inválida, digite novamente!')
+            questions[2]()
+            return;
+        }
+
         workCordinates({ 
             x: answer[0],
             y: answer[1],
             d: answer[2] 
         })
+
         questions[3]()
     }),
 
     3: () => readLine.question("Defina o movimento da sonda: ", answer => {    
         const actualIndex = cordinates.length - 1
-        if(answer.includes(!'M' || !'P' || !'L' || !'R')) return;
+        let orders = {
+            'M': () => workMovement(actualIndex),
+            'P': () => {
+                console.log('Fotografando area')
+                setTimeout(() => {
+                    console.log('Fotografado!')
+                },500 
+            )},
+            'L': () => workDirections('L',actualIndex),
+            'R': () => workDirections('R',actualIndex)
+        }
 
-        answer.forEach(order => {
-            
-            if(order === 'M'){
-                workMovement(actualIndex)
-            }
+        for(const order of answer){
+            if(!['L','R','M','P'].includes(order)) return;
+            orders[order]()
+        }
 
-        } )
-            
         questions[4]()
     }),
 
