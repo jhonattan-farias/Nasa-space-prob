a2adf41cc3e57279d1586260a4fa4598c9992efd
Jhonattan Farias <63968305+jhonattan-farias@users.noreply.github.com>
2022-08-23 21:11:36 -0300

    finished the basics functions

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { createInterface } = require('readline');
 
 
-let cordinates = { x: 0, y: 0, d:'N' };
+let cordinates = [];
 let highlandSize = { x:0, y: 0 }
 
 
@@ -13,22 +13,21 @@ let highlandSize = { x:0, y: 0 }
 // adicionar a direção que a sonda esta apontada [X]
 // adicionar posição atual da sonda [X]
 // adicionar um movimento a sonda [X]
-// adicionar linhas de entrada para adicionar mais sondas []
+// adicionar linhas de entrada para adicionar mais sondas [X]
 
-function workMovement() {
-
+function workMovement(index) {
     let movement = {
-        'S': () => cordinates.y--,
-        'W': () => cordinates.x--,
-        'E': () => cordinates.x++,
-        'N': () => cordinates.y++
+        'S': () => cordinates[index].y--,
+        'W': () => cordinates[index].x--,
+        'E': () => cordinates[index].x++,
+        'N': () => cordinates[index].y++
     }
 
-    movement[cordinates.d]()
-    console.log(cordinates)
+    movement[cordinates[index].d]()
+    console.log(cordinates[index])
 }
 
-function workDirections(inputDirection) {
+function workDirections(inputDirection,index) {
 
     if(inputDirection === 'L'){
         let changedDirection = {
@@ -37,7 +36,7 @@ function workDirections(inputDirection) {
             'N':'W',
             'W':'S'
         }
-        cordinates.d = changedDirection[inputDirection];
+        cordinates[index].d = changedDirection[inputDirection];
     }
 
     if(inputDirection === 'R'){
@@ -47,7 +46,7 @@ function workDirections(inputDirection) {
             'N':'E',
             'E':'S'
         }
-        cordinates.d = changedDirection[inputDirection];
+        cordinates[index].d = changedDirection[inputDirection];
     }
 }
 
@@ -55,9 +54,8 @@ function workCordinates({ x, y, d }) {
     // if(x > highlandSize.x || x - 0) return;
     // if(y > highlandSize.y || y - 0) return;
     
-    cordinates.x = Number(x) + cordinates.x;
-    cordinates.y = Number(y) + cordinates.y;
-    cordinates.d = d;
+    cordinates.push({ x, y, d})
+    console.log(cordinates)
 }
 
 const readLine = createInterface({
@@ -67,12 +65,12 @@ const readLine = createInterface({
 
 const questions = {
     0: () => readLine.question("Defina o tamanho do eixo x do planalto: ", answer => {
-        highlandSize.x = answer;
+        highlandSize.x = answer
         questions[1]()
     }),
     
-    1: () => readLine.question("Defina o tamanho do y do planalto: ", answer => {
-        highlandSize.y = answer;
+    1: () => readLine.question("Defina o tamanho do eixo y do planalto: ", answer => {
+        highlandSize.y = answer
         questions[2]()
     }),
 
@@ -86,7 +84,13 @@ const questions = {
     }),
 
     3: () => readLine.question("Defina o movimento da sonda: ", answer => {
-            workMovement()
+            const actualIndex = cordinates.length - 1
+            workMovement(actualIndex)
+            questions[4]()
+    }),
+
+    4: () => readLine.question("Inserir mais uma sonda? s = (sim) n = (não): ", answer => {
+        answer === 's' ? questions[2]() : readLine.close();
     })
 }
 
